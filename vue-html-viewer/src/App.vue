9c2a4ad769663dadd5d56ffa90ca236687f86f12
Todor Ivanov <todor.ivanov@primeholding.com>
2020-01-25 11:32:27 +0200

    Initial project setup

---
+++ vue-html-viewer/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HtmlPreview></HtmlPreview>
+  </div>
+</template>
+
+<script>
+import HtmlPreview from "./components/html-preview/HtmlPreview";
+export default {
+  name: "app",
+  components: {
+    HtmlPreview
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
